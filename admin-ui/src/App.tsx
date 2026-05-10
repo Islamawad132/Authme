392cdb9c76b01426cd37af79a32dcb5320efc179
Islam Ahmed <islamahmedas12@gmail.com>
2026-05-10 16:23:56 +0300

    auto-claude: subtask-5-1 - Create ConsentCategoriesListPage component

--- admin-ui/src/App.tsx
+++ admin-ui/src/App.tsx
@@ -23,6 +23,7 @@ import IdpDetailPage from './pages/identity-providers/IdpDetailPage';
 import ClientScopeListPage from './pages/client-scopes/ClientScopeListPage';
 import ClientScopeCreatePage from './pages/client-scopes/ClientScopeCreatePage';
 import ClientScopeDetailPage from './pages/client-scopes/ClientScopeDetailPage';
+import ConsentCategoriesListPage from './pages/consent/ConsentCategoriesListPage';
 import FederationListPage from './pages/user-federation/FederationListPage';
 import FederationCreatePage from './pages/user-federation/FederationCreatePage';
 import FederationDetailPage from './pages/user-federation/FederationDetailPage';
@@ -71,6 +72,7 @@ export default function App() {
           <Route path="/console/realms/:name/client-scopes" element={<ClientScopeListPage />} />
           <Route path="/console/realms/:name/client-scopes/create" element={<ClientScopeCreatePage />} />
           <Route path="/console/realms/:name/client-scopes/:scopeId" element={<ClientScopeDetailPage />} />
+          <Route path="/console/realms/:name/consent-categories" element={<ConsentCategoriesListPage />} />
           <Route path="/console/realms/:name/events" element={<LoginEventsPage />} />
           <Route path="/console/realms/:name/admin-events" element={<AdminEventsPage />} />
           <Route path="/console/realms/:name/user-federation" element={<FederationListPage />} />
