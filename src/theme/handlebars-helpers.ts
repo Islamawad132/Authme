be0b06c4b350baae4b33ca174e7a2495d71cc71f
Islam Ahmed <islamahmedas12@gmail.com>
2026-05-13 18:15:41 +0300

    fix(theme): extend eslint-disable for handlebars-helpers unsafe-assignment
    - Handlebars helpers inherently use 'any' typed options object

--- src/theme/handlebars-helpers.ts
+++ src/theme/handlebars-helpers.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-assignment -- Handlebars helper types are inherently any */
 import hbs from 'hbs';
 
 /**
